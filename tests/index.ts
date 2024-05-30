a0ce77c76028c5287059b0edd52c3d46e8f47cec
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-05-30 10:40:39 -0700

    uvic co-op data scraped, some data not getting scraped properly throughout the tables

--- tests/index.ts
+++ tests/index.ts
@@ -1,11 +1,34 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-import { Browser } from 'puppeteer';
+const path = require('path');
+const fs = require('fs');
+import { Browser, Page } from 'puppeteer';
 puppeteer.use(StealthPlugin());
 
-const URL = 'https://learninginmotion.uvic.ca/myAccount/co-op/postings.htm'
-const LOGIN = 'uvic_netlink_id'
-const PASSWORD = 'password_uvic_netlink_id'
+const LOGGED_IN_URL_0 = 'https://learninginmotion.uvic.ca/myAccount/dashboard.htm'
+const LOGGED_IN_URL_1 = 'https://learninginmotion.uvic.ca/myAccount/co-op/postings.htm'
+const LOGGED_OUT_URL = 'https://learninginmotion.uvic.ca/notLoggedIn.htm'
+const LOGIN = 'uvic-username'
+const PASSWORD = 'uvic-password'
+const COOKIES_PATH = path.resolve(__dirname, 'cookies.json');
+
+
+async function appendDataToFile(filename, data) {
+    let existingData = [];
+    try {
+        const fileContents = fs.readFileSync(filename, 'utf-8');
+        existingData = JSON.parse(fileContents);
+        if (!Array.isArray(existingData)) {
+            existingData = [];
+        }
+    } catch (err) {
+        // File does not exist or is empty
+    }
+    const randomArrayName = Math.random().toString(36).substring(7); // Generate random array name
+    existingData.push({ [randomArrayName]: data });
+    fs.writeFileSync(filename, JSON.stringify(existingData, null, 4));
+}
+
 
 /**
  * Function that logs into the Learning in Motion website and navigates
@@ -16,9 +39,15 @@ const main = async () => {
     const page = await browser.newPage();                                                                       // Create a new page
     const pages = await browser.pages();                                                                        // Get all open pages
     if (pages.length > 1) { await pages[0].close(); }                                                           // Close the first page if it is not the login page
-    await page.goto(URL);                                                                                       // Navigate to the main page
 
-    if (page.url() === 'https://learninginmotion.uvic.ca/notLoggedIn.htm') {                                    // If the user is not logged in, navigate to the login page
+    if (fs.existsSync(COOKIES_PATH)) {                                                                          // If the cookies file exists
+        const cookies = JSON.parse(fs.readFileSync(COOKIES_PATH, 'utf8'));                                      // Read the cookies from the file
+        await page.setCookie(...cookies);                                                                       // Set the cookies on the page
+    }
+
+    await page.goto(LOGGED_IN_URL_0);                                                                           // Navigate to the main page
+
+    if (page.url() === LOGGED_OUT_URL) {                                                                        // If the user is not logged in, navigate to the login page
         const studentsLink = await page.waitForSelector('a[href="/students/NetlinkID/student-login.htm"]');     // Find the link to the student login page
         if (studentsLink) { await studentsLink.click(); }                                                       // If the link exists, click it
         await page.waitForSelector('input[name="username"]');                                                   // Wait for the username input field to load
@@ -42,9 +71,62 @@ const main = async () => {
                     break;
                 }
             }
-            if (page.url() === 'https://learninginmotion.uvic.ca/myAccount/dashboard.htm') { break; }           // If the user is logged in, break the loop
+
+            if (page.url() === LOGGED_IN_URL_0 || page.url() === LOGGED_IN_URL_1) {                             // If the user is logged in
+                const cookies = await page.cookies();                                                           // Save cookies after successful login
+                fs.writeFileSync(COOKIES_PATH, JSON.stringify(cookies, null, 2));                               // Write the cookies to the file
+                break;                                                                                          // If the user is logged in, break the loop
+            }
         }
-    } else { await new Promise(resolve => setTimeout(resolve, 5000)); }                                         // If the user is already logged in, wait for 5 seconds
-    await browser.close();                                                                                      // closes the ChromiumTest browser
+    } else {
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        await page.goto(LOGGED_IN_URL_1);
+
+        page.evaluate(() => {
+            const link = Array.from(document.querySelectorAll('a')).find(el => el.innerText.includes('2024 - Fall'));
+            if (link) link.click();
+        });
+
+        await page.waitForSelector('tbody');
+        const rows = await page.$$('tbody tr');
+
+        for (const row of rows) {
+            const [newPage] = await Promise.all([
+                new Promise<Page>(resolve =>
+                    browser.once('targetcreated', async target => {
+                        const newPage = await target.page();
+                        if (newPage) resolve(newPage);
+                    })
+                ),
+                row.$eval('a[class^="np-view-btn-"]', (anchor: HTMLAnchorElement) => anchor.click())
+            ]);
+
+            await newPage.waitForSelector('.panel.panel-default .panel-body');
+
+            const data = await newPage.evaluate(() => {
+                const result = {};
+                const rows = Array.from(document.querySelectorAll('.panel.panel-default .panel-body table tbody tr'));
+
+                for (const row of rows) {
+                    const cells = row.querySelectorAll('td');
+                    if (cells.length >= 2) {
+                        const key = cells[0]?.innerText?.trim().replace(/:/g, '');
+                        const value = cells[1]?.innerText?.trim().replace(/:/g, '');
+                        if (key) {
+                            result[key] = value ?? "N/A";
+                        }
+                    }
+                }
+
+                return result;
+            });
+
+            page.waitForNavigation({ waitUntil: 'networkidle0' });
+            await appendDataToFile('result.json', data);
+            console.log('[ Data appended ]');
+            await newPage.close();
+        }
+    }
+    await browser.close();
 }
 main()
